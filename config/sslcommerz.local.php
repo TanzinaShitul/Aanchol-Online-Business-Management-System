a7336f87f2f79d3a3edd3ec065fb699d8582089e
Tanzina Akther <shitultanzina@gmail.com>
2026-07-21 14:16:02 +0600

    ssl local setup

---
+++ config/sslcommerz.local.php
@@ -0,0 +1,8 @@
+<?php
+// Local SSLCommerz sandbox credentials. This file is excluded from Git.
+return [
+    'store_id' => 'aanch6a5ef88b7ae07',
+    'store_password' => 'aanch6a5ef88b7ae07@ssl',
+    'is_sandbox' => true,
+    'base_url' => '',
+];
